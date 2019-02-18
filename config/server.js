370906f49dfcec9f1ad84530306b984ae606966f
Sachin Jadhav <sachin0321@gmail.com>
2019-02-18 11:10:49 +0530

    Added all files

---
+++ config/server.js
@@ -0,0 +1,5 @@
+module.exports = {
+    APPLICATION_ID: process.env.APPLICATION_ID || '0613228ae8741eed81a0a24c0d4a6f71e477a367c8375a8c37ce4850e7213384',
+    SECRET: process.env.SECRET || '1586f3d7b501ccf36866f5818c3895d8e00059354d7e61a490d2b84e617ed7b9',
+    CALLBACK_URL: process.env.CALLBACK_URL || 'http://localhost:3000'
+}
